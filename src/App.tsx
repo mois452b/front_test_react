04d5c76149a593b9e859786afe42a6ae52e6193d
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2024-02-16 07:48:57 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { ImageInfoComponent } from './components/Image'
+import { ImageInfo } from './components/ImageInfo'
+import { MarvelMoviesSeries } from './components/MarvelMoviesSeries'
+import { Weather } from './components/weather'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <ImageInfoComponent />
+      {/* <ImageInfo /> */}
+      <MarvelMoviesSeries />
+      {/* <Weather /> */}
     </>
   )
 }
