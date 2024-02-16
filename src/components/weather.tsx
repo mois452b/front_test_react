1ea4b12192ea3570212a995bfdf01335a940a367
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2024-02-15 23:24:32 -0400

    refactoring weather component view

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -1,53 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import { gethWeatherData } from '../utils/getWeatherData';
+import { getForecastData } from '../utils/getForecastData';
+
+export interface WeatherDataInterface {
+    name: string;
+    temp: number;
+    description: string;
+    humidity: number;
+    pressure: number;
+    wind_speed: number;
+}
 
 export const Weather = () => {
-  const [weatherData, setWeatherData] = useState([]);
+  const [weatherData, setWeatherData] = useState<WeatherDataInterface[]>([]);
   const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchWeatherData = async ( cities: string[] ) => {
-            try {
-                const apiKey = 'b9cc4f4f3510e8c6130611073844c1e6';
+            const datas = await gethWeatherData( cities );
+            setWeatherData(datas);
+            setLoading(false);
 
-                const datas = await Promise.all(
-                    cities.map( async city => {
-                        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-                        const data = await response.json();
-                        return data
-                    })
-                );
-                console.log(datas)
-                setWeatherData( datas );
-                setLoading(false);
-
-                
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-                setLoading(false);
-            }
         };
 
         const fetchWeatherForecast = async ( cities: string[], days: number ) => {
-            try {
-                const apiKey = 'b9cc4f4f3510e8c6130611073844c1e6';
-                const datas = await Promise.all(
-                    cities.map( async city => {
-                        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=${days}&appid=${apiKey}`)
-                        const data = await response.json();
-                        return data
-                    })
-                )
-                console.log(datas)
-            }
-            catch( error ) {
-                console.error('Error fetching weather forecast:', error);
-            }
+            const datas = await getForecastData( cities, days );
+            console.log( datas)
         }
 
-        const cities = ['London', 'Paris', 'New York', 'Tokyo', 'Sydney', 'Caracas'];
+        const cities = ['London', 'Paris', 'Tokyo', 'Sydney', 'Caracas'];
         fetchWeatherData( cities );
         fetchWeatherForecast( cities, 1 );
-        fetchWeatherForecast( cities, 2 );
+        // fetchWeatherForecast( cities, 2 );
     }, []);
 
   if (loading) {
@@ -64,11 +48,10 @@ export const Weather = () => {
         {weatherData.map((cityData, index) => (
             <div key={index} className="my-4 p-4 border border-gray-300 rounded-md">
             <h2 className="text-xl font-semibold">Weather in {cityData.name}</h2>
-            <p className="mt-2">Temperature: {cityData.main.temp} °C</p>
-            <p>Weather: {cityData.weather[0].description}</p>
-            <p>Humidity: {cityData.main.humidity}%</p>
-            <p>Pressure: {cityData.main.pressure} hPa</p>
-            {/* Add more weather details as needed */}
+            <p className="mt-2">Temperature: {cityData.temp} °C</p>
+            <p>Weather: {cityData.description}</p>
+            <p>Humidity: {cityData.humidity}%</p>
+            <p>Pressure: {cityData.pressure} hPa</p>
             </div>
         ))}
         </div>
