3560f57e01247ba21e9f5c13b9998e005c4bd454
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2024-02-16 11:22:00 -0400

    fix buf

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -15,6 +15,8 @@ export interface WeatherDataInterface {
 export const Weather = () => {
   const [isOpen, setIsOpen] = useState( false )
   const [weatherData, setWeatherData] = useState<WeatherDataInterface[]>([]);
+  const [weatherForecast1, setWeatherForecast1] = useState<WeatherDataInterface[]>([]);
+  const [weatherForecast2, setWeatherForecast2] = useState<WeatherDataInterface[]>([]);
   const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -25,41 +27,61 @@ export const Weather = () => {
 
         };
 
-        const fetchWeatherForecast = async ( cities: string[], days: number ) => {
-            const datas = await getForecastData( cities, days );
-            console.log( datas)
+        const fetchWeatherForecast = async ( cities: string[] ) => {
+            const datas1 = await getForecastData( cities, 1 );
+            setWeatherForecast1(datas1);
+            const datas2 = await getForecastData( cities, 2 );
+            setWeatherForecast2(datas2);
         }
 
         const cities = ['London', 'Paris', 'Tokyo', 'Sydney', 'Caracas'];
         fetchWeatherData( cities );
-        fetchWeatherForecast( cities, 1 );
-        // fetchWeatherForecast( cities, 2 );
+        fetchWeatherForecast( cities );
     }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-  if (!weatherData.length) {
-    return <div>No weather data available</div>;
-  }
+    if (!weatherData.length) {
+        return <div>No weather data available</div>;
+    }
 
   return (
     <div>
         <button onClick={() => setIsOpen(true)} className='bg-gradient-to-bl from-blue-300 to-blue-500 text-white hover:from-blue-500 hover:to-blue-700 px-4 py-2 rounded-md flex flex-row gap-2'>
-          <span>Ver Climas</span>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z" />
-          </svg>
+            <span>Ver Climas</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z" />
+            </svg>
         </button>
         <Modal isOpen={isOpen} onRequestClose={()=>setIsOpen(false)}>
-        <h2 className="text-2xl font-bold text-center mb-4">Weather</h2>
+            <div>
+                <h2 className="text-2xl font-bold text-center mb-4">Current Weather</h2>
                 <div className="flex flex-wrap justify-center items-start gap-4">
                     {
                         weatherData.map( (item, index) => <DisplayWeather key={index} city={item} /> )
                     }
                 </div>
-                <button className="bg-red-500 text-white px-4 py-2 mt-4 rounded" onClick={() => setIsOpen(false)}>Cerrar</button>
+            </div>
+            <div>
+                <h2 className="text-2xl font-bold text-center mb-4">1 day weather forecast</h2>
+                <div className="flex flex-wrap justify-center items-start gap-4">
+                    {
+                        weatherForecast1.map( (item, index) => <DisplayWeather key={index} city={item} /> )
+                    }
+                </div>
+            </div>
+            <div>
+                <h2 className="text-2xl font-bold text-center mb-4">2 days weather forecast</h2>
+                <div className="flex flex-wrap justify-center items-start gap-4">
+                    {
+                        weatherForecast2.map( (item, index) => <DisplayWeather key={index} city={item} /> )
+                    }
+                </div>
+            </div>
+
+            <button className="bg-red-500 text-white px-4 py-2 mt-4 rounded" onClick={() => setIsOpen(false)}>Cerrar</button>
 
         </Modal>
       </div>
